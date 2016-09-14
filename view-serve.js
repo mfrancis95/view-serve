42d580329f9aad1afa5cbb22e56e4cb7cf3df584
mfrancis95 <mikefrancis95@gmail.com>
2016-09-14 12:24:54 -0400

    Initial commit

---
+++ view-serve.js
@@ -0,0 +1,22 @@
+var fs = require("fs");
+var path = require("path");
+
+function middleware(directory, locals) {
+    return function(request, response, next) {
+        var view = request.url.slice(1);
+        if (!view) {
+            view = "index";
+        }
+        var file = path.join(directory, view);
+        fs.stat(path.join(request.app.get("views"), directory, view + ".ejs"), function(error) {
+            if (error) {
+                next();
+            }
+            else {
+                response.render(file, locals[view]);
+            }
+        });
+    };
+}
+
+module.exports = middleware;
